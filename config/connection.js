e349d6293c4e4773cf4aad5e7f77952c09f4c607
“anar <sehar.uzair23@gmail.com>
2023-05-24 22:24:15 -0300

    mongoose

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/videosAndResponses');
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/aggregateDB');
 
-module.exports = connection;
+// Export connection 
+module.exports = mongoose.connection;
