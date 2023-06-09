c67fb8fc1cfb5e9a0dd624714ff96450586927e5
“anar <sehar.uzair23@gmail.com>
2023-06-09 14:12:54 -0300

    bug fixed

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,7 +7,7 @@ const {
   deleteThought,
   createReaction,
   deleteReaction,
-} = require("../../controllers/thought-controller");
+} = require("../../controllers/thoughtController");
 
 // /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
