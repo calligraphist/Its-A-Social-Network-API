70bca23cf0c4a3ca02620d27249d18f3e34ade19
“anar <sehar.uzair23@gmail.com>
2023-06-09 11:49:11 -0300

    thought model and reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const {Schema, Types, model}=require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+    // Mongoose's ObjectId data type
+        type: Schema.Types.ObjectId,
+    // Default value is set to a new ObjectId   
+        default:()=> new Types.ObjectId()
+    },
+    reactionBody:{
+        type:String, required:true, maxlength:280
+    },
+    username:{
+        type:String, required:true
+    },
+    //todo: add created at
+},
+{ 
+    toJSON:{
+        virtuals: true, getters:true
+    },
+     id:false
+})
+
+
+const thoughtSchema =new mongoose.Schema({
+    thoughtText: {type:String, required:true, minlength: 1, maxlength:280},
+    username:{type:String, required:true},
+    reactions:[reactionSchema]
+      //todo: add created at
+},
+{
+    toJSON:{virtuals: true, getters: true},
+    id:false
+});
+
+thoughtSchema.virtual("reactionCount").get(function(){
+    return this.reactions.length;
+});
+
+const {Thought}=model("Thought", thoughtSchema)
+
+module.exports = Thought;
