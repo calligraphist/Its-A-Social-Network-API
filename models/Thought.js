fb2c8184eea84c1c008ac4e8f27bd0f19ec8a2db
“anar <sehar.uzair23@gmail.com>
2023-06-09 14:24:02 -0300

    new schema

--- models/Thought.js
+++ models/Thought.js
@@ -16,7 +16,7 @@ const reactionSchema = new Schema({
 });
 
 
-const thoughtSchema =new mongoose.Schema({
+const thoughtSchema =new Schema({
     thoughtText: {type:String, required:true, minlength: 1, maxlength:280},
     username:{type:String, required:true},
     reactions:[reactionSchema]
